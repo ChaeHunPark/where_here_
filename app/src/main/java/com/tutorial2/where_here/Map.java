f5dcf8c023008db7459cb738845fae786bd462c4
kukjin <kjin@kakao.com>
2022-04-18 18:56:54 +0900

    setting gps

--- app/src/main/java/com/tutorial2/where_here/Map.java
+++ app/src/main/java/com/tutorial2/where_here/Map.java
@@ -4,26 +4,52 @@
 
 package com.tutorial2.where_here;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class Map extends AppCompatActivity {
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
 
     public int count =0;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.map_f_btn);
         LinearLayout bar = (LinearLayout)findViewById(R.id.menu_bar);
+
+//위치설정
+        LocationManager locationManager;
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map_fragment);
+
+//getMapAsync를 호출하여 비동기로 onMapReady 콜백 매서드 호출(네이버 플렛폼 참고)
+        if(mapFragment == null){
+            mapFragment = MapFragment.newInstance();
+            fragmentManager.beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
 
 //fab를 눌렀을때 이벤트 처리를 위한 온클릭 리스너 장착
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +57,6 @@ public class Map extends AppCompatActivity {
             public void onClick(View view) {
                 if(count%2 == 0) {
                     bar.setVisibility(View.VISIBLE);
-//                    //버튼 이미지와 색상 변경
                     fab.setRotation(45);
                 }else{
                     bar.setVisibility(View.GONE);
@@ -40,5 +65,28 @@ public class Map extends AppCompatActivity {
                 count++;
             }
         });
+
+    }
+
+//navermap setting
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap=naverMap;
+        naverMap.setLocationSource(locationSource);
+        naverMap.getUiSettings().setLocationButtonEnabled(true);
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Face);
+    }
+
+//현재 위치 사용권한 요청
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,  @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) { //요청 허용if문
+            if(!locationSource.isActivated()){
+//요청이 거부된 경우
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
