8fae6a14b8d99db4d2893eb2ec83f1040e3207e1
ChaeHun.Park <cogns8571@naver.com>
2022-05-18 13:45:53 +0900

    dd

--- app/src/main/java/com/tutorial2/where_here/Info.java
+++ app/src/main/java/com/tutorial2/where_here/Info.java
@@ -6,67 +6,113 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.tutorial2.where_here.Info_Strings.Info_Strings;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+
 public class Info extends AppCompatActivity {
 
     public Info_Strings info_strings = new Info_Strings();
     public static int Rides_Index;
+    public static StringBuilder sb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
-        ListView listView = (ListView) findViewById(R.id.list_info); // 리스트의 아이디
-        ListitemAdapter_Info adapter = new ListitemAdapter_Info(); // adapter에 어댑터 클래스 참조
-        listView.setAdapter(adapter); // 찾은 아이디에 어댑터 지정
 
-        for (int i = 0; i < 29; i++){
-            adapter.addItem(new Listitem_Info(info_strings.info_title[i],info_strings.info_intro[i],
-                    info_strings.info_images[i])); // 커스텀 리스트 add
+        //read
+        try{
+            InputStream is = getBaseContext().getResources().getAssets().open("where_rides.xls");
+            Workbook wb = Workbook.getWorkbook(is);
+
+            if(wb != null){
+                Sheet sheet = wb.getSheet(0);
+
+                if(sheet != null){
+                    int colTotal = sheet.getColumns();
+                    int rowStart = 1;
+                    int rowTotal = sheet.getColumn(colTotal-1).length;
+
+
+                    for(int row = rowStart; row<rowTotal; row++){
+                        sb = new StringBuilder();
+
+                        for(int col = 0; col < colTotal; col++){
+                            String contents = sheet.getCell(col,row).getContents();
+                            sb.append("col" +col+ ":"+contents+" , ");
+                        }
+                        Log.i("test",sb.toString());
+                    }
+                }
+            }
+
+
+        } catch (IOException | BiffException e) {
+            e.printStackTrace();
         }
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { // 인덱스 클릭했을때 이벤트 지정
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Listitem_Info item = (Listitem_Info) parent.getItemAtPosition(position); //item에 포지션 반환
-
-                String title = item.getTitle();
-                String intro = item.getIntro();
-
-                for(int i = 0; i<29; i++) {
-
-                    if (title.substring(0) == info_strings.info_title[i]) { // 다이얼로그
-                        Rides_Index = i; //Info_to_Map에서 인덱스으로 값 사용
-                        AlertDialog.Builder builder = new AlertDialog.Builder(Info.this);
-                        builder.setTitle(title);
-                        builder.setMessage(intro);
-                        builder.setPositiveButton(info_strings.info_title[i]+" 타러가기", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent intent = new Intent(Info.this, Info_to_Map.class);
-                                startActivity(intent);
-                            }
-                        });
-                        builder.setNeutralButton("닫기", null);
-                        builder.create().show();
-                    }
-
-                }
+        Log.i("Hello",sb.toString());
 
 
-            }
-        });
+    }
+
+//        ListView listView = (ListView) findViewById(R.id.list_info); // 리스트의 아이디
+//        ListitemAdapter_Info adapter = new ListitemAdapter_Info(); // adapter에 어댑터 클래스 참조
+//        listView.setAdapter(adapter); // 찾은 아이디에 어댑터 지정
+//
+//        for (int i = 0; i < 29; i++){
+//            adapter.addItem(new Listitem_Info(info_strings.info_title[i],info_strings.info_intro[i],
+//                    info_strings.info_images[i])); // 커스텀 리스트 add
+//        }
+//
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { // 인덱스 클릭했을때 이벤트 지정
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Listitem_Info item = (Listitem_Info) parent.getItemAtPosition(position); //item에 포지션 반환
+//
+//                String title = item.getTitle();
+//                String intro = item.getIntro();
+//
+//                for(int i = 0; i<29; i++) {
+//
+//                    if (title.substring(0) == info_strings.info_title[i]) { // 다이얼로그
+//                        Rides_Index = i; //Info_to_Map에서 인덱스으로 값 사용
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(Info.this);
+//                        builder.setTitle(title);
+//                        builder.setMessage(intro);
+//                        builder.setPositiveButton(info_strings.info_title[i]+" 타러가기", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialog, int which) {
+//                                Intent intent = new Intent(Info.this, Info_to_Map.class);
+//                                startActivity(intent);
+//                            }
+//                        });
+//                        builder.setNeutralButton("닫기", null);
+//                        builder.create().show();
+//                    }
+//
+//                }
+//
+//
+//            }
+//        });
 
 
 //        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
 //        jsoupAsyncTask.execute();
-    }
+
 
 //    @Override
 //    protected void onPause() {
