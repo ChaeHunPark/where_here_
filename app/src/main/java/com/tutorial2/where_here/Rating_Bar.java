74558fa53452ae2d1b12aeaa38c1acd5a99c1907
ChaeHun.Park <cogns8571@naver.com>
2022-06-06 03:17:44 +0900

    star save

--- app/src/main/java/com/tutorial2/where_here/Rating_Bar.java
+++ app/src/main/java/com/tutorial2/where_here/Rating_Bar.java
@@ -1,14 +1,45 @@
 package com.tutorial2.where_here;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class Rating_Bar extends AppCompatActivity {
+
+    private static String IP_ADDRESS = "서버 IP주소";
+    private static String TAG = "phpsignup";
+    private boolean validate = false;
+
+    private AlertDialog dialog;
+    private TextView mTextViewResult;
 
     private RatingBar ratingBar;
     private TextView textView;
@@ -27,9 +58,120 @@ public class Rating_Bar extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                float rating = ratingBar.getRating();
-                textView.setText(String.valueOf(rating));
+                double score = ratingBar.getRating(); // 레이팅 값
+                textView.setText(String.valueOf(score));
+
+                double sc = Double.parseDouble(textView.getText().toString());
+
+                InsertData task = new InsertData();
+                task.execute("http://192.168.112.70/rating/insert.php", sc + "");
+                textView.setText("");
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Rating_Bar.this);
+                dialog = builder.setMessage("별점 제출 완료")
+                        .setCancelable(false)
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+
+
+                        .create();
+                dialog.show();
+
             }
         });
+    }
+
+
+    class InsertData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(Rating_Bar.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            progressDialog.dismiss();
+            Log.d(TAG, "POST response  - " + result);
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String score = (String) params[1];
+
+
+            String serverURL = (String) params[0];
+            String postParameters = "score=" + score;
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+
+                return new String("Error: " + e.getMessage());
+
+
+            }
+
+        }
     }
 }
